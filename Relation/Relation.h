81527c00bea59d121bcf618fafa13a8b507ff4a9
Rob Rix <rob.rix@github.com>
2014-12-14 10:56:16 -0500

    Initial Commit

---
+++ Relation/Relation.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+/// Project version number for Relation.
+extern double RelationVersionNumber;
+
+/// Project version string for Relation.
+extern const unsigned char RelationVersionString[];
+
